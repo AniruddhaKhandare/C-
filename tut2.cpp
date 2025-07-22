022f91b65ee08bb0574944dc2bf2f28e2e1bd98e
Aniruddha Khandare <99129410+AniruddhaKhandare@users.noreply.github.com>
2025-07-22 12:42:47 +0530

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+//This code was created by Aniruddha
+/*this
+is 
+done 
+__BYTE_ORDER__
+sagale lakshat thev*/ 
+int main(){
+    int sum = 10;
+    cout<<"Hello World"<< sum;
+    return 0;
+}
