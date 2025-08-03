0b4eb564e5594fc3373b84012f0f1ec03ad13add
Aniruddha Khandare <99129410+AniruddhaKhandare@users.noreply.github.com>
2025-08-03 17:55:01 +0530

    Add files via upload

---
+++ tut16.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+inline int product(int a, int b)
+{
+    // Not recommended to use below lines with inline functions
+    //static int c=0;      //This executes only once
+    // c = c+1;      //Next time this function is run,The value of c will be retained
+    //return a*b+c;  //dont use static in inline use only in simple function
+    return a * b;
+}
+
+float moneyRecieved(int currentMoney, float factor = 1.04)
+{ //default arg first and then compulsory arg;
+    return currentMoney * factor;
+}
+//int strlen(const char *p){
+
+//}
+int main()
+{
+    int a, b;
+    //cout<<"Enter the value of a and b";
+    //cin>>a>>b;
+    //cout<<"The product of a and b is "<<product(a,b)<<endl;
+    int money = 100000;
+    cout << "If you have" << money << "Rs in your bank account,you will recieve" << moneyRecieved(money) << "Rs after 1 year" << endl;
+    cout << "For VIP: If you have" << money << "Rs in your bank account,you will recieve" << moneyRecieved(money, 1.10) << "Rs after 1 year";
+
+    return 0;
+}
