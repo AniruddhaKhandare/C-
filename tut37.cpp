a465dced1afd9005a7a548bc22d9c808b23d7116
Aniruddha Khandare <overcastbulb@gmail.com>
2025-08-11 13:44:18 +0530

    Add files via upload

---
+++ tut37.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+//Base class
+class Employee
+{
+public:
+    int id;
+    float salary;
+    Employee(int inpId)
+    {
+        id = inpId;
+        salary = 34.0;
+    }
+    Employee() {}
+};
+// Derived class syntax
+/*
+class {{derived-class-name}} : {{visibility-mode}} {{base-class-name}}
+{
+    class members/methods/etc...
+}
+Note:
+1.Default visibility mode is private
+2.Public Visibility Mode: Public members of the base class becomes Public members of the derived class
+3.Private Visibility Mode: Public members of the base class becomes Private members of the derived class
+4.Private members are never inherited
+*/
+class Programmer : Employee
+{
+public:
+    int languageCode;
+    Programmer(int inpId)
+    {
+        id = inpId;
+        languageCode = 9;
+    }
+
+    void getData(){
+        cout<<id<<endl;
+    }
+};
+
+//Creating a Programmer class derived from Employee Base class
+
+int main()
+{
+    Employee harry(1), rohan(2);
+    cout << harry.salary << endl;
+    cout << rohan.salary << endl;
+    Programmer skillF(10);
+    cout << skillF.languageCode<<endl;
+    skillF.getData();
+    return 0;
+}
