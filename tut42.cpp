5ef3b14424a8c2fa719f57be407596d565c2d06c
Aniruddha Khandare <overcastbulb@gmail.com>
2025-08-12 16:51:43 +0530

    Add files via upload

---
+++ tut42.cpp
@@ -0,0 +1,21 @@
+#include<iostream> // (Done in tut47.cpp)
+
+using namespace std;
+/*
+Create 2 classes:
+    1. SimpleCalculator - Takes input of 2 numbers using a utility function and performs +,-,*,/. and displays 
+    the result using another function.
+    2. ScientificCalculator - Takes input of 2 numbers using a utility function and performs any four scientific 
+    operation of your choice and displays the results using another function.
+
+    Create another class HybridCalculator and inherit it using these 2 classess:
+    Q1. What type of Inheritance are you using?
+    Q2. Which mode of Inheritance are you using?
+    Q3. Create an object of HybridCalculator and display results of simple and scientific calculator.
+    Q4. How is code reusability implemented?
+    Ans - It is called code reusability because we reuse it.
+    */
+int main(){
+    
+    return 0;
+}
