55dc8cf98a48f6e502a0323f31851a1b1038785d
Aniruddha Khandare <overcastbulb@gmail.com>
2025-08-13 17:35:44 +0530

    Add files via upload

---
+++ tut47.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+/*
+Create 2 classes:
+    1. SimpleCalculator - Takes input of 2 numbers using a utility function and performs +,-,*,/. and displays 
+    the result using another function.
+    2. ScientificCalculator - Takes input of 2 numbers using a utility function and performs any four scientific 
+    operations of your choice and displays the results using another function.
+
+    Create another class HybridCalculator and inherit it using these 2 classess:
+    Q1. What type of Inheritance are you using? ---->Multiple Inheritance
+    Q2. Which mode of Inheritance are you using? ---->public SimpleCalculator , public ScientificCalculator
+    Q3. Create an object of HybridCalculator and display results of simple and scientific calculator.
+    Q4. How is code reusability implemented?
+    Ans - It is called code reusability because we reuse it.
+    */
+class SimpleCalculator{
+    public:
+    int a,b;
+    void getDataSimple(){
+        cout<<"Enter the value of a"<<endl;
+        cin>>a;
+        cout<<"Enter the value of b"<<endl;
+        cin>>b;
+    }
+
+    void performOperationsSimple(){
+            cout<<"The value of a + b is: "<<a + b<<endl;
+            cout<<"The value of a - b is: "<<a - b<<endl;
+            cout<<"The value of a * b is: "<<a * b<<endl;
+            cout<<"The value of a / b is: "<<a / b<<endl;
+    }
+    
+};
+
+class ScientificCalculator{
+    public:
+    int a,b;
+    void getDataScientific(){
+        cout<<"Enter the value of a"<<endl;
+        cin>>a;
+        cout<<"Enter the value of b"<<endl;
+        cin>>b;
+    }
+
+    void performOperationsScientific(){
+            cout<<"The value of cos(a) is: "<<cos(a)<<endl;
+            cout<<"The value of sin(a) is: "<<sin(a)<<endl;
+            cout<<"The value of exp(a) is: "<<exp(a)<<endl;
+            cout<<"The value of tan(a) is: "<<tan(a)<<endl;
+    }
+};
+
+class HybridCalculator : public SimpleCalculator , public ScientificCalculator{
+};
+
+int main(){
+    //SimpleCalculator calc;
+    //ScientificCalculator calc;
+    //calc.getData();
+    //calc.performOperations();
+    HybridCalculator calc;
+    calc.getDataScientific();
+    calc.performOperationsScientific();
+    calc.getDataSimple();
+    calc.performOperationsSimple();
+    return 0;
+}
